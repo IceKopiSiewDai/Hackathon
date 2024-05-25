4567de54a48d0c45acddac8e268d619e1883d7ac
ngjunrong <118435186+ngjunrong@users.noreply.github.com>
2024-05-25 17:25:00 +0800

    minor changes to past events

--- frontend/src/Pages/ViewEvent/viewevent.js
+++ frontend/src/Pages/ViewEvent/viewevent.js
@@ -1,47 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import "./viewevent.css"; // Import CSS for styling
-
-// const ViewEvent = () => {
-//   const [eventGroups, setEventGroups] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch past events from backend
-//     fetchPastEvents();
-//   }, []);
-
-//   const fetchPastEvents = async () => {
-//     try {
-//       const response = await fetch("http://localhost:5000/event/getPastEvents");
-//       if (!response.ok) {
-//         throw new Error("Failed to fetch events");
-//       }
-//       const eventData = await response.json();
-//       setEventGroups(eventData);
-//     } catch (error) {
-//       console.error("Error fetching events:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="event-grid-container">
-//       {eventGroups.map((eventGroup, index) => (
-//         <div key={index} className="event-group">
-//           {eventGroup.map((event) => (
-//             <div key={event._id} className="event-card">
-//               <h3>{event.title}</h3>
-//               <p>{event.description}</p>
-//               <p>Date: {new Date(event.date).toLocaleDateString()}</p>
-//               <p>Time: {event.time}</p>
-//               <img src={event.image} alt="Event" />{" "}
-//             </div>
-//           ))}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ViewEvent;
 import React, { useState, useEffect } from "react";
 import "./viewevent.css";
 import NavBar from "../Navbar/navbar";
@@ -79,9 +35,27 @@ const ViewEvent = () => {
               <div key={event._id} className="event-card">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
-                <p>Date: {new Date(event.date).toLocaleDateString()}</p>
-                <p>Time: {event.time}</p>
-                <img src={event.image} alt="Event" />{" "}
+                <p>
+                  Location:{" "}
+                  <a
+                    href={event.location}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Google Maps Link
+                  </a>
+                </p>{" "}
+                <p>Genre: {event.genre}</p>{" "}
+                <p>
+                  Date:{" "}
+                  {new Date(event.date).toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+                <p>Start Time: {event.time}</p>
+                <img src={event.image} alt="Event" />
               </div>
             ))}
             {/* Fill up empty spaces for maintaining 3x3 grid */}
